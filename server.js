90baeef44d98bfabb67c37403ad5c14545ed9aab
simone-sampino <s.samp98@gmail.com>
2025-06-30 17:47:50 +0200

    feat: add recipe posts and corresponding images to the blog

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
+const postRouter = require("./routers/posts");
 
 app.use(express.static("public"));
 
@@ -12,39 +13,39 @@ app.get("/", (req, res) => {
   res.send("Server del mio blog");
 });
 
-app.get("/api/posts", (req, res) => {
-  res.json(posts);
-});
+app.use("/api/posts", postRouter);
 
+/* 
 const posts = [
   {
     title: "Ciambellone",
     content: "Ciambellone classico",
-    image: `http://localhost:${PORT}/desserts/ciambellone.jpeg`,
+    image: `http://localhost:${PORT}/recipes/ciambellone.jpeg`,
     tags: ["dolce", "semplice", "ciambellone"],
   },
   {
     title: "Cracker",
     content: "Cracker alla barbabietola",
-    image: `http://localhost:${PORT}/desserts/cracker_barbabietola.jpeg`,
+    image: `http://localhost:${PORT}/recipes/cracker_barbabietola.jpeg`,
     tags: ["dolce", "semplice", "barbabietola"],
   },
   {
     title: "Pane Fritto",
     content: "Pane fritto dolce",
-    image: `http://localhost:${PORT}/desserts/pane_fritto_dolce.jpeg`,
+    image: `http://localhost:${PORT}/recipes/pane_fritto_dolce.jpeg`,
     tags: ["dolce", "fritto", "nowaste"],
   },
   {
     title: "Pasta",
     content: "Pasta alla barbabietola",
-    image: `http://localhost:${PORT}/desserts/pasta_barbabietola.jpeg`,
+    image: `http://localhost:${PORT}/recipes/pasta_barbabietola.jpeg`,
     tags: ["dolce", "pasta", "barbabietola"],
   },
   {
     title: "Torta",
     content: "Torta Paesana",
-    image: `http://localhost:${PORT}/desserts/torta_paesana.jpeg`,
+    image: `http://localhost:${PORT}/recipes/torta_paesana.jpeg`,
     tags: ["dolce", "torta", "tradizione"],
   },
 ];
+ */
